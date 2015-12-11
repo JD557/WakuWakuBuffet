d1c20ee364ecd1a39365519f0fc4c3ca5659e8dd
João Costa <joao.costa@shiftforward.eu>
2015-12-11 22:45:38 +0000

    Add dummy canvas code

---
+++ src/js/engine.js
@@ -0,0 +1,5 @@
+function get2dContext() {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  return ctx;
+}
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+const ctx = get2dContext();
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 100, 100);
