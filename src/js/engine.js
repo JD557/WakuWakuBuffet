6082efd0e4e40c735001025d14e089cb6c8bcd11
João Costa <joao.costa@shiftforward.eu>
2015-12-12 11:39:16 +0000

    Add basic key handling

--- src/js/engine.js
+++ src/js/engine.js
@@ -3,3 +3,41 @@ function get2dContext() {
   const ctx = canvas.getContext('2d');
   return ctx;
 }
+
+const btn1 = 65; // A
+const btn2 = 90; // Z
+
+var btn1Pressed = false;
+var btn2Pressed = false;
+
+function baseKeyHandler(e) {
+    if (e.keyCode == btn1) {
+      btn1Pressed = e.type == 'keydown';
+    }
+    else if (e.keyCode == btn2) {
+      btn2Pressed = e.type == 'keydown';
+    }
+}
+
+var btn1Callback = function() {console.log("btn1");};
+var btn2Callback = function() {console.log("btn2");};
+var btn12Callback = function() {console.log("both");};
+
+onkeyup = function(e) {
+  e = e || event;
+  baseKeyHandler(e);
+}
+
+onkeydown = function(e) {
+  e = e || event;
+  if (!(e.keyCode == btn1 && btn1Pressed) && !(e.keyCode == btn2 && btn2Pressed)) { // Ignore key repeat
+    baseKeyHandler(e);
+    if (btn1Pressed && btn2Pressed) {
+      btn12Callback();
+   }
+    else {
+      if (e.keyCode == btn1) {btn1Callback();}
+    else if (e.keyCode == btn2) {btn2Callback();}
+    }
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 const ctx = get2dContext();
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 100, 100);
+
+var playerY = 10;
+
+btn1Callback = () => playerY -= 10;
+btn2Callback = () => playerY += 10;
+
+function main() {
+  ctx.clearRect(0, 0, 640, 480);
+  ctx.fillStyle = 'green';
+  ctx.fillRect(10, playerY, 100, 100);
+  requestAnimationFrame(main);
+};
+
+main();
